cfd12d9868e973cda19c8ad1536015202e12c3d9
KhoaVo <vokhoa408@gmail.com>
2012-11-26 18:09:22 -0800

    Fixed toObject to return nested defaultdicts

--- defaultdict.js
+++ defaultdict.js
@@ -1,27 +1,76 @@
+var findDefaultDicts = function(obj){
+    var res = {},count = 0;
+    
+    for(var k in obj){
+        if(obj[k].toObject){
+            res[k] = 1;
+            count++;
+        }
+    }
+    if(count)
+        return res;
+    else
+        return null;
+}
+
 var defaultdict = function(def) {
+    var val,testVal,d
+    if(typeof def === 'function'){
+        testVal = def('__defaultdict__');
+        if(testVal === Object(testVal) && !Array.isArray(testVal)){
+            d = findDefaultDicts(testVal);
+            if(d){
+                val = function(key){
+                    var v = def(key);
+                    v.__dicts = d;
+                    return v;
+                }
+            }else
+                val = def;
+        }else{
+            val = def;
+        }
+    }
+    else
+        val = function(){return def;};
+    
     var dict = function(key) {
         if (key in dict)
             return dict[key];
-        if (typeof def === "function"){
-            dict[key] = def(key);
-            return dict[key];
-        }
             
-        dict[key] = def;
-        return def;
+        dict[key] = val(key);
+        return dict[key];
     };
     
     dict.toObject = function(){
-        var ret = {},cur;
+        var ret = {},cur,k2,temp;
+        var toObject = dict.toObject;
+        var dicts = dict.__dicts;
+        
+        delete dict.toObject;
+        delete dict.__dicts;
+        
         for(var key in dict){
-            if(key !== "toObject"){
-                cur = dict[key];
-                if(cur.toObject)
-                    cur = cur.toObject();
+            cur = dict[key];
+            if(cur.toObject)
+                cur = cur.toObject();
+            else if (cur.__dicts){
+                temp = {};
+                for(k2 in cur)
+                    temp[k2] = cur[k2];
                 
-                ret[key] = cur;
+                for(k2 in cur.__dicts){
+                    temp[k2] = cur[k2].toObject();
+                }
+
+                delete temp.__dicts;
+                cur = temp;
             }
+            ret[key] = cur;
         }
+        
+        dict.toObject = toObject;
+        dict.__dicts = dicts;
         
         return ret;
     }
